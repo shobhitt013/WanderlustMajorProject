a5158d3b5545053702295df982add8d1b4fd4af0
SHOBHIT SHARMA <shobhit130905@gmail.com>
2026-07-04 14:50:48 +0530

    Refactor initDB function for better DB handling
    Refactor database initialization logic to include connection handling and error management.

--- init/index.js
+++ init/index.js
@@ -1,29 +1,61 @@
+// const mongoose = require("mongoose");
+// const initData = require("./data.js");
+// const Listing = require("../models/listing.js");
+
+// //const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// require("dotenv").config();
+
+// const MONGO_URL = process.env.ATLASDB_URL;
+
+// main()
+//     .then(() => {
+//         console.log("connected to DB");
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// async function main() {
+//     await mongoose.connect(MONGO_URL);
+// }
+
+// const initDB = async() => {
+//     await Listing.deleteMany({});
+//     initData.data = initData.data.map((obj) => ({...obj, owner: "690f18e88f5c30803149de83" }));
+//     await Listing.insertMany(initData.data);
+//     console.log("data was initialized");
+// }
+
+// initDB();
+
+require("dotenv").config();
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-//const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-require("dotenv").config();
-
 const MONGO_URL = process.env.ATLASDB_URL;
 
-main()
-    .then(() => {
-        console.log("connected to DB");
-    })
-    .catch((err) => {
+async function initDB() {
+    try {
+        await mongoose.connect(MONGO_URL);
+        console.log("Connected to DB");
+
+        await Listing.deleteMany({});
+
+        initData.data = initData.data.map((obj) => ({
+            ...obj,
+            owner: "690f18e88f5c30803149de83",
+        }));
+
+        await Listing.insertMany(initData.data);
+
+        console.log("Data was initialized");
+
+        await mongoose.connection.close();
+        console.log("Connection closed");
+    } catch (err) {
         console.log(err);
-    });
-
-async function main() {
-    await mongoose.connect(MONGO_URL);
-}
-
-const initDB = async() => {
-    await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner: "690f18e88f5c30803149de83" }));
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized");
+    }
 }
 
 initDB();
